86b3f186d0bef516bf996be313c3a028da34979a
Leona Christen Macedo <leo.christenmacedo@gmail.com>
2022-06-14 09:39:03 -0300

    [feat]: base CRUD operations | errors treatment | clients

--- back-end/src/clients/clients.service.ts
+++ back-end/src/clients/clients.service.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Knex } from 'knex';
 import { InjectModel } from 'nest-knexjs';
-import { UpdateClientDto } from './dto/update-client.dto';
+import { UpdateClientAddressDto } from './dto/update-client.dto';
 import { Client } from './entities/client.entity';
 
 @Injectable()
@@ -11,29 +11,44 @@ export class ClientsService {
 
   private readonly table = 'shopper_clients';
 
-  async create(client: Client) {
+  async create(client: Client):Promise<string>{
     const {id, name, email, address} = client;
 
     if (!id || !name || !email || !address) {
         throw new HttpException('Verify the fields: name, price, email and address, all of them must be filled', HttpStatus.BAD_REQUEST);
     }
 
-    return await this.connection(this.table).insert(client);
-  }
+    await this.connection(this.table).insert(client);
+    return 'Client created';
+  };
 
-  findAll() {
-    return `This action returns all clients`;
-  }
+  async findAll():Promise<Client[]>{
+    return await this.connection(this.table);
+  };
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
-  }
+  async findOne(id: string):Promise<Client>{
+    const client: Client[] = await this.connection(this.table).where({id});
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
-  }
+    if (!client[0]) throw new HttpException('Client not found, please check if you passed a valid client id', HttpStatus.NOT_FOUND);
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
-  }
+    return client[0];
+  };
+
+  async updateAddress(id: string, input: UpdateClientAddressDto):Promise<string>{
+    const {address} = input;
+
+    if (!address) throw new HttpException('Please check if you have passed a address', HttpStatus.BAD_REQUEST);
+
+    await this.connection(this.table).where({id}).update({address});
+    return 'Client address updated';
+  };
+
+  async remove(id: string):Promise<string>{
+    const client: Client = await this.findOne(id);
+
+    if (!client) throw new HttpException('Client not found, please check if you passed a valid client id', HttpStatus.NOT_FOUND);
+
+    await this.connection(this.table).where({id}).delete();
+    return 'Client removed';
+  };
 }
